fd3935b1dc49149a46fd04e8dd7c14a0f3770681
iyu88 <iyu88887@gmail.com>
2022-04-13 11:59:48 +0900

    Week6

--- public/Week6/week6.js
+++ public/Week6/week6.js
@@ -1,28 +1,31 @@
-/*
 import * as THREE from "/scripts/three/build/three.module.js";
 import { OrbitControls } from "/scripts/three/examples/jsm/controls/OrbitControls.js";
 
+const videoElement = document.getElementsByClassName("input_video")[0];
+const canvasElement = document.getElementsByClassName("output_canvas")[0];
+const canvasCtx = canvasElement.getContext("2d");
+
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setViewport(0, 0, window.innerWidth, window.innerHeight);
+const $width = 640;
+const $height = 480;
+renderer.setSize($width, $height);
+renderer.setViewport(0, 0, $width, $height);
 document.body.appendChild(renderer.domElement);
 
-const camera = new THREE.PerspectiveCamera(
-  45,
-  window.innerWidth / window.innerHeight,
-  1,
-  500
-);
+const camera = new THREE.PerspectiveCamera(45, $width / $height, 1, 500);
 camera.position.set(0, 0, 100);
 camera.lookAt(0, 0, 0);
 camera.up.set(0, 1, 0);
 
 // Orbit 생성
 // camera 에 대한 작업이 더 이상 필요하지 않음
-const controls = new OrbitControls(camera, renderer.domElement);
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableZoom = false;
+// controls.enablePan = false;
 
 const scene = new THREE.Scene();
 
+/*
 const points = [];
 points.push(new THREE.Vector3(-10, 0, 0));
 points.push(new THREE.Vector3(0, 10, 0));
@@ -69,10 +72,6 @@ function animate() {
 animate();
 */
 
-const videoElement = document.getElementsByClassName("input_video")[0];
-const canvasElement = document.getElementsByClassName("output_canvas")[0];
-const canvasCtx = canvasElement.getContext("2d");
-
 function onResults(results) {
   canvasCtx.save();
   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
@@ -85,6 +84,11 @@ function onResults(results) {
   );
   if (results.multiFaceLandmarks) {
     for (const landmarks of results.multiFaceLandmarks) {
+      // 가상의 projection matrix 를 통해서 카메라의 intrinsic parameter 를 몰라도 camera space 에 좌표를 저장
+      // 정규화하여 -1 ~ 1 사이의 값으로 변환됨 & 원점을 중심으로 하여 정규화된 점들을 반환
+      // camera space 로 unproject 하면 near plane 주변에 잡히게 됨
+      // 카메라로부터 scaling backprojection 을 수행
+      // 3d to 2d 호모그래피 : w term -> 이미지 거리를 조절하는 역할 (카메라가 위치한 좌표만큼 떨어트림)
       drawConnectors(canvasCtx, landmarks, FACEMESH_TESSELATION, {
         color: "#C0C0C070",
         lineWidth: 1,
@@ -114,21 +118,24 @@ function onResults(results) {
     }
   }
   canvasCtx.restore();
+  renderer.render(scene, camera); // renderer 추가
 }
 
 const faceMesh = new FaceMesh({
   locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`;
+    return `/scripts/@mediapipe/face_mesh/${file}`;
   },
 });
 faceMesh.setOptions({
-  maxNumFaces: 1,
-  refineLandmarks: true,
-  minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5,
+  maxNumFaces: 1, // Mesh 의 수
+  refineLandmarks: true, // iris tracking ON / OFF
+  minDetectionConfidence: 0.5, // vertex (landmark points) 들에 대해서 내부적인 확실성의 하한
+  minTrackingConfidence: 0.5, // 내부적인 확실성의 상한 (threshold)
 });
-faceMesh.onResults(onResults);
+faceMesh.onResults(onResults); // Mesh 의 landmark 를 전달
 
+// 비디오를 매 프레임마다 프로세싱 -> camera_utils 사용
+/*
 const camera = new Camera(videoElement, {
   onFrame: async () => {
     await faceMesh.send({ image: videoElement });
@@ -136,4 +143,12 @@ const camera = new Camera(videoElement, {
   width: 1280,
   height: 720,
 });
-camera.start();
+*/
+// camera.start();
+
+async function processVideo() {
+  await faceMesh.send({ image: videoElement });
+  videoElement.requestVideoFrameCallback(processVideo);
+}
+
+processVideo();
